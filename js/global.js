3c43cb86e85e45847ff917446c5b80e2fb57b3bf
Pete Dunlap <pdunlap@shareableink.com>
2015-06-22 17:17:27 -0500

    first customization, now extension can block webpages i specify in background.js

--- js/global.js
+++ js/global.js
@@ -3,3 +3,5 @@
 ** description: global javascript library containing code for use across the entire chrome extension, for
 **              unique functionality for specific pages, create and use a separate file named "pagename.js"
 */
+debugger
+alert($("body"));
---
+++ js/steps.js
@@ -0,0 +1,2 @@
+// find a way to get the number of steps completed that day from fitbit api only using javascript
+
