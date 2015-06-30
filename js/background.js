53f0ec0f6ad219d470693f90b81e73d3be83bf42
Pete Dunlap <pdunlap@shareableink.com>
2015-06-30 16:25:05 -0500

    WIP instructions for next commit

--- js/background.js
+++ js/background.js
@@ -1,7 +1,9 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
 
-      if (parseInt(localStorage.getItem("steps")) > parseInt(localStorage.getItem("goal"))) {
+// HOW OFTEN SHOULD WE GET NEW STEPS NUMBERS!!??
+// everytime this runs!  this only runs when the url matches anyways
+      if (localStorage.getItem("steps") != null && localStorage.getItem("goal") != null && (parseInt(localStorage.getItem("steps")) > parseInt(localStorage.getItem("goal")))) {
         return {};
       } else {
         chrome.tabs.update(details.tabId, {url: chrome.extension.getURL('/html/disallowed.html')});
