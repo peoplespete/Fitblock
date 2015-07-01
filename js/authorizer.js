064f5a1e38d1ed91b5f1d122851ab0594ec17ff4
Pete Dunlap <pdunlap@Petes-MacBook-Pro.local>
2015-07-01 07:45:24 -0500

    moves token retrieval to authorizer.js

--- js/authorizer.js
+++ js/authorizer.js
@@ -8,23 +8,52 @@ function getParameterByName(name) {
 
 localStorage.setItem("code", getParameterByName('code'));
 
-var missing_config = false;
-if(localStorage.getItem("client_id") == null){
-  missing_config = true;
-  alert("Missing Fitbit Client ID");
-}
-if(localStorage.getItem("secret") == null){
-  missing_config = true;
-  alert("Missing Fitbit Secret");
-}
-if(localStorage.getItem("code") == null || localStorage.getItem("code") == ""){
-  missing_config = true;
-  alert("Missing Fitbit Code");
-}
-if(missing_config){
-  alert("Problem with your Fitbit Registration, try again.");
-  window.location.replace(chrome.extension.getURL('/html/options.html'));
-}
-else{
-  $("#message").text("Fitbit Registration Complete" );      
-}
+url = "https://api.fitbit.com/oauth2/token?";
+url = url + "client_id=" + localStorage.getItem("client_id") + "&";
+url = url + "grant_type=" + "authorization_code" + "&";
+url = url + "redirect_uri=" + chrome.extension.getURL('/html/authorizer.html') + "&";
+url = url + "code=" + localStorage.getItem("code");
+
+$.ajax({
+  url: url,
+  type: 'POST',
+  beforeSend: function (xhr) {
+    xhr.setRequestHeader('Authorization', 'Basic ' + window.btoa(unescape(encodeURIComponent(localStorage.getItem("client_id") + ':' + localStorage.getItem("secret")))));
+  },
+  contentType: 'application/x-www-form-urlencoded',
+
+  success: function (data) {
+    
+    localStorage.setItem("access_token", data.access_token);
+
+    var missing_config = false;
+    if(localStorage.getItem("client_id") == null){
+      missing_config = true;
+      alert("Missing Fitbit Client ID");
+    }
+    if(localStorage.getItem("secret") == null){
+      missing_config = true;
+      alert("Missing Fitbit Secret");
+    }
+    if(localStorage.getItem("code") == null || localStorage.getItem("code") == ""){
+      missing_config = true;
+      alert("Missing Fitbit Code");
+    }
+    if(localStorage.getItem("access_token") == null || localStorage.getItem("access_token") == ""){
+      missing_config = true;
+      alert("Missing Fitbit Access Token");
+    }
+    if(missing_config){
+      alert("Problem with your Fitbit Registration, try again.");
+      window.location.replace(chrome.extension.getURL('/html/options.html'));
+    }
+    else{
+      $("#message").text("Fitbit Registration Complete" );      
+    }
+
+  },
+  error: function (e) {
+    alert(e);
+    console.log(e);
+  },
+});
--- js/background.js
+++ js/background.js
@@ -18,34 +18,33 @@ chrome.webRequest.onBeforeRequest.addListener(
         missing_config = true;
         alert("Missing Fitbit Secret");
       }
-      if(localStorage.getItem("code") == null){
+      if(localStorage.getItem("code") == null || localStorage.getItem("code") == ""){
         missing_config = true;
         alert("Missing Fitbit Code");
+      }
+      if(localStorage.getItem("access_token") == null || localStorage.getItem("access_token") == ""){
+        missing_config = true;
+        alert("Missing Fitbit Access Token");
       }
       if(missing_config){
         chrome.tabs.update(details.tabId, {url: chrome.extension.getURL('/html/disallowed.html')});
       }
-
-      url = "https://api.fitbit.com/oauth2/token?";
-      url = url + "client_id=" + localStorage.getItem("client_id") + "&";
-      url = url + "grant_type=" + "authorization_code" + "&";
-      url = url + "redirect_uri=" + chrome.extension.getURL('/html/authorizer.html') + "&";
-      url = url + "code=" + localStorage.getItem("code");
-
       $.ajax({
 
-        url: url,
-        type: 'POST',
+        url: "https://api.fitbit.com/1/user/-/activities/goals/daily.json",
+        type: 'GET',
         beforeSend: function (xhr) {
           xhr.setRequestHeader('Authorization', 'Basic ' + window.btoa(unescape(encodeURIComponent(localStorage.getItem("client_id") + ':' + localStorage.getItem("secret")))));
+          xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem("access_token"));
         },
         contentType: 'application/x-www-form-urlencoded',
 
         success: function (data) {
-          localStorage.setItem("access_token", data.access_token);
-          $.ajax({
+          console.log("Daily Goal:" + data.goals.steps);
+          localStorage.setItem("goal", data.goals.steps);
 
-            url: "https://api.fitbit.com/1/user/-/activities/goals/daily.json",
+          $.ajax({
+            url: "https://api.fitbit.com/1/user/-/activities/date/" + date_today() + ".json",
             type: 'GET',
             beforeSend: function (xhr) {
               xhr.setRequestHeader('Authorization', 'Basic ' + window.btoa(unescape(encodeURIComponent(localStorage.getItem("client_id") + ':' + localStorage.getItem("secret")))));
@@ -54,51 +53,34 @@ chrome.webRequest.onBeforeRequest.addListener(
             contentType: 'application/x-www-form-urlencoded',
 
             success: function (data) {
-              console.log("Daily Goal:" + data.goals.steps);
-              localStorage.setItem("goal", data.goals.steps);
-
-              $.ajax({
-                url: "https://api.fitbit.com/1/user/-/activities/date/" + date_today() + ".json",
-                type: 'GET',
-                beforeSend: function (xhr) {
-                  xhr.setRequestHeader('Authorization', 'Basic ' + window.btoa(unescape(encodeURIComponent(localStorage.getItem("client_id") + ':' + localStorage.getItem("secret")))));
-                  xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem("access_token"));
-                },
-                contentType: 'application/x-www-form-urlencoded',
-
-                success: function (data) {
-                  console.log("Steps Count:" + data.summary.steps);
-                  localStorage.setItem("steps", data.summary.steps);
-                  var percentage_completion = Math.floor((localStorage.getItem("steps") / localStorage.getItem("goal")) * 100);
-                  console.log(percentage_completion);
-                  if (localStorage.getItem("steps") != null && localStorage.getItem("goal") != null && (parseInt(localStorage.getItem("steps")) > parseInt(localStorage.getItem("goal")))) {
-                    return {};
-                  } else {
-                    chrome.tabs.update(details.tabId, {url: chrome.extension.getURL('/html/disallowed.html')});
-                    // return {cancel: true};
-                  }
-                },
-                error: function (e) {
-                  
-                  console.log("failure to get step count!!!");
-                  console.log(e);
-                  chrome.tabs.update(details.tabId, {url: chrome.extension.getURL('/html/disallowed.html')});
-                },
-              });
-
+              console.log("Steps Count:" + data.summary.steps);
+              localStorage.setItem("steps", data.summary.steps);
+              var percentage_completion = Math.floor((localStorage.getItem("steps") / localStorage.getItem("goal")) * 100);
+              console.log(percentage_completion);
+              if (localStorage.getItem("steps") != null && localStorage.getItem("goal") != null && (parseInt(localStorage.getItem("steps")) > parseInt(localStorage.getItem("goal")))) {
+                return {};
+              } else {
+                chrome.tabs.update(details.tabId, {url: chrome.extension.getURL('/html/disallowed.html')});
+                // return {cancel: true};
+              }
             },
             error: function (e) {
-              console.log("failure to get goal!!!");
-              console.log(e);
-              if(e.responseText.includes("Access token invalid or expired")){
-                window.location.replace(chrome.extension.getURL("/html/options.html"));
-              }
+              alert(e.responseText);      
+              console.log("failure to get step count!!!");
+              console.log(e.responseText);
+              chrome.tabs.update(details.tabId, {url: chrome.extension.getURL('/html/disallowed.html')});
             },
           });
 
         },
         error: function (e) {
-          console.log(e);
+          alert(e.responseText);
+          console.log("failure to get goal!!!");
+          console.log(e.responseText);
+          if(e.responseText.includes("Access token invalid or expired")){
+            localStorage.clear()
+            window.location.replace(chrome.extension.getURL("/html/options.html"));
+          }
         },
       });
 
